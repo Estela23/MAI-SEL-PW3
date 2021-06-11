334c281b4e550f21c5e024c417eb010dd05524c4
FernandoVN98 <38290736+FernandoVN98@users.noreply.github.com>
2021-06-11 16:18:17 +0200

    Primeras versiones menu y leer json

--- read_constraints.py
+++ read_constraints.py
@@ -9,7 +9,7 @@ def read_parse_json(filename):
     print(categories)
     glasses=set([child.find('glasstype').text for child in cocktails])
     ingredients=set([child.find('ingredients').find('ingredient').text for child in cocktails])
-    alc_type = set([child.find('ingredients').find('ingredient').attrib['alc_type'] for child in cocktails])
+    alc_types = set([child.find('ingredients').find('ingredient').attrib['alc_type'] for child in cocktails])
     with open(filename) as json_file:
         data = json.load(json_file)
     if len(data.keys())==0:
@@ -33,6 +33,20 @@ def read_parse_json(filename):
                     aux=aux+1
         if 'alc_type' not in data['constraints']:
             data['constraints']['alc_type']=[]
+        else:
+            aux=0
+            for idx, alc_type in enumerate(data['constraints']['alc_type']):
+                if alc_type not in alc_types:
+                    data['constraints']['alc_type'].remove(idx-aux)
+                    aux=aux+1
+        if 'exc_ingredients' not in data['constraints']:
+            data['constraints']['exc_ingredients']=[]
+        else:
+            aux=0
+            for idx, exc_ingredients in enumerate(data['constraints']['exc_ingredients']):
+                if exc_ingredients not in ingredients or exc_ingredients in data['constraints']['ingredients']:
+                    data['constraints']['alc_type'].remove(idx-aux)
+                    aux=aux+1
     return data['constraints']
 
 #TODO: ELIMINAR ESTO DESPUES EH
