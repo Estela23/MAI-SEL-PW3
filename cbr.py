3259b4499aa2054f3cc403fb4839e83fa51c9269
xaviercucurull <xavierqq@gmail.com>
2021-06-11 17:55:41 +0200

    Create CBR class skeleton
    Related to issue #16

---
+++ cbr.py
@@ -0,0 +1,26 @@
+"""
+
+PW3 - SEL - 2021
+CBR for a cocktail recipe creator
+
+Authors:    Xavier Cucurull Salamero <xavier.cucurull@estudiantat.upc.edu>
+            Daniel Hinjos García <daniel.hinjos@estudiantat.upc.edu>
+            Fernando Vázquez Novoa <fernando.vazquez.novoa@estudiantat.upc.edu>
+            Estela Vázquez-Monjardín Lorenzo <estela.vazquez-monjardin@estudiantat.upc.edu>
+"""
+
+from lxml import etree
+import numpy as np
+
+class CBR:
+    def __init__(self, name, cbl_filename):
+        return
+    
+    def _init_structure(self):
+        return
+    
+    def retrieval(self):
+        return
+    
+    def adaptation(self):
+        return
