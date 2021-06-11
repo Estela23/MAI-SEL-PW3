7bdc5046a57f17b527632cc4793e779bb5b19cba
Estela23 <estela.monxardina@gmail.com>
2021-06-11 13:01:07 +0200

    tiny changes

--- adaptation.py
+++ adaptation.py
@@ -1,11 +1,12 @@
-def adapt_step(best_case, constraints):
-    solution = {"name": constraints["name"] if "name" in constraints else str(best_case["name"]) + "2.0",
-                "category": constraints["category"] if "category" in constraints else best_case["category"],
-                "glasstype": constraints["glasstype"] if "glasstype" in constraints else best_case["glasstype"]}
+def adaptation_step(best_case, constraints):
+    solution = {"name": constraints["name"] if "name" in constraints else best_case.find("name").text + "2.0",
+                "category": constraints["category"] if "category" in constraints else best_case.find("category").text,
+                "glasstype": constraints["glasstype"] if "glasstype" in constraints else best_case.find("glasstype").text}
 
     # Adapt the ingredients:
     # Add ingredients
-    # TODO: hacer esto al revés para tener las cantidades, meter primero los de la receta y luego los que añadimos
+    # TODO: hacer esto al revés para tener las cantidades, meter primero los de la receta
+    #  y luego los que añadimos metiendo las cantidades al chou (?)
     ingredients = constraints["ingredients"]    # TODO: quantity? index?
     for ingr in best_case["ingredients"]:
         # Remove ingredients if they belong to "forbidden_ingredients"
