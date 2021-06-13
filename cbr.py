dec6295c0d0eb86d48b602e1510352c0a36cddca
Xavier <xavierqq@gmail.com>
2021-06-13 14:03:45 +0200

    Revert "Get unique categories and name in constraint not a list"

--- cbr.py
+++ cbr.py
@@ -49,9 +49,7 @@ class CBR:
         Parse cocktails tree and extract relevant information such as 
         the unique categories, alcohol types, ingredients, etc.
         """
-        # Get unique categories of cocktail
-        self.categories = set([c.find('category').text for c in self.cocktails])
-        
+
         # Get unique values for alcohol types
         self.alcohol_types.update(
             [i.attrib['alc_type'] for i in self.cocktails.findall('cocktail/ingredients/ingredient')])
@@ -463,8 +461,8 @@ class CBR:
         n_changes = 0
 
         # Change the name of the cocktail according to the constraints
-        if len(constraints["name"]):
-            adapted_cocktail.find("name").text = constraints["name"]
+        if len(constraints["cocktail_name"]):
+            adapted_cocktail.find("name").text = constraints["cocktail_name"]
         else:
             adapted_cocktail.find("name").text += "2.0"
 
@@ -601,4 +599,19 @@ class CBR:
         else:
             adapted_cocktail.find('evaluation').text = "Failure"
         return adapted_cocktail, score
+'''
+# To test RETRIEVAL step
+constraints = {'category': ['Cocktail', 'Shot'], 'glasstype': ['Beer glass', 'Shot glass'], 'ingredients': ['Amaretto'],
+                'alc_type': ['Rum'], 'basic_type': ['Sweet'], 'exc_ingredients': ['Vodka']}
+
+
+
+
+    '''
+    # To test RETRIEVAL step
+    constraints = {'category': ['Cocktail', 'Shot'], 'glasstype': ['Beer glass', 'Shot glass'], 'ingredients': ['Amaretto'],
+                    'alc_type': ['Rum'], 'basic_type': ['Sweet'], 'exc_ingredients': ['Vodka']}
     
+    cbr = CBR("Data/case_library.xml")
+    case_retrieved = cbr.retrieval(constraints)
+    '''
